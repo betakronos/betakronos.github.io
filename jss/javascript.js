576e52b28ec4d1b4e022cd9656ffdb898a4392df
betakronos <118102023+betakronos@users.noreply.github.com>
2023-02-26 21:06:14 -0300

    Add files via upload

---
+++ jss/javascript.js
@@ -0,0 +1,11 @@
+var tienCajaUno = document.getElementById('caja_productos');
+
+function mostrarCajaUno() {
+tienCajaUno.style.display = 'block'
+}
+
+
+var cerrarTien = document.getElementById('caja_productos')
+
+function cerrarTienda(){
+    cerrarTien.style.display = 'none'}
---
+++ jss/javascript2.js
@@ -0,0 +1,11 @@
+var nombreUsuario = prompt('¿Cual es tu nombre?')
+
+alert('Hola '+ nombreUsuario)
+
+var edad = prompt("ingrese su edad")
+if(edad >= 18){
+    alert("Bienvenido/a "+ nombreUsuario)
+}
+else{
+    alert("Debes ser mayor a 18 para ingresar a la página")
+}
